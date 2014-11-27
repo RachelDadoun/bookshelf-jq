ccc5d0a41fb88aa7f3b984e4e9630dd6860ff277
RachelDadoun <RachelDadoun@users.noreply.github.com>
2014-11-27 19:01:33 +0200

    Create targil.js

---
+++ targil.js
@@ -0,0 +1,18 @@
+$(document).on("ready", function(){
+
+buildEvents();
+
+function buildEvents (){
+
+
+BookEvent(".adddBookForm button:nth(1)", "click", "addBook" );
+
+
+};
+function BookEvents(selector,eventName,functionName){
+
+};
+
+
+
+});
