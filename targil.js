7c1103a3ba0546ed77cc41724bef7580681968fd
RachelDadoun <RachelDadoun@users.noreply.github.com>
2014-11-27 19:51:19 +0200

    Update targil.js

--- targil.js
+++ targil.js
@@ -1,18 +1,42 @@
 $(document).on("ready", function(){
+window.Books=[];
+window.seq=0;
 
-buildEvents();
+$(".adddBookForm button:nth(1)").on("click",function(){
 
-function buildEvents (){
+newBook = Book ($(".adddBookForm input:nth(0)").val(),$(".adddBookForm input:nth(1)").val(), $(".adddBookForm input:nth(1)").val())
+books.push(newBook);
+BuildBookList();
 
+});
 
-BookEvent(".adddBookForm button:nth(1)", "click", "addBook" );
-
-
+function Book(bookName, bookAuthor, bookScore){
+this.bookName = bookName;
+this.bookAuthor = bookAuthor;
+this.bookScore =  bookScore;
+this.id= "book-" + seq++;
 };
-function BookEvents(selector,eventName,functionName){
 
+function BuildBookList(){
+
+  for(var i=0;i <Books.length; I++)
+  {
+    AddBookToLi(i);
+  }
+  
 };
 
+function AddBookToLi(i){
+    FillTemplate();
+    AddTemplate();
+    $("ul")append("#template");
+    $("li:last-nth").attr("id", Books[i].id);
+    $("#" + Books[i].id).append("<label>" + Books[i].bookName + "</label>");
+    $("#" + Books[i].id).append("<label>" + Books[i].bookAuthor + "</label>");
+    $("#" + Books[i].id).append("<label>" + Books[i].bookScore + "</label>");
+     $("#" + Books[i].id).append("<button>" + Books[i].bookScore + "</button>");
+  
+};
 
 
 });
